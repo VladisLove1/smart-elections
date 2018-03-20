92798dadcdaf469ff1512ceffe3bcb66999a1193
VladisLove1 <vladislove0212@gmail.com>
2018-03-21 00:28:17 +0200

    added js, removed garb items

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 App = {
   web3Provider: null,
   contracts: {},
+  account: "0x0",
 
   init: function() {
     return App.initWeb3();
@@ -11,38 +12,64 @@ App = {
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
     } else {
-      App.web3Provider = web3.providers.HttpProvider('http://localhost:9545');
+      App.web3Provider = web3.providers.HttpProvider('http://localhost:8545');
       web3 = new Web3(App.web3Provider);
     }
     return App.initContract();
   },
 
   initContract: function() {
-    $.getJSON("Election.json", function(election) {
-      
-    })
+    $.getJSON('Election.json', function(election) {
+      //Instantiate a new truffle contract from the artifact
+      App.contracts.Election = TruffleContract(election);
+      //connect provider to interact with contract
+      App.contracts.Election.setProvider(App.web3Provider);
+    });
+    return App.render();
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
+  render: function() {
+    let electionInstance;
+    let loader = $("#loader");
+    let content = $("#content");
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
+    loader.show();
+    content.hide();
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+    //load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if(err == null) {
+        App.account = account;
+        $("#accountAddress").html("Your account: " + account);
+      }
+    });
 
-    var petId = parseInt($(event.target).data('id'));
+    //load contract data
+    App.contracts.Election.deployed().then(function(i) {
+      electionInstance = i;
+      return electionInstance.candidatesCount();
+    }).then(function(candidatesCount){
+      let candidatesResults = $("#candidatesResults");
+      candidatesResults.empty();
 
-    /*
-     * Replace me...
-     */
+      for(let i = 1; i <= candidatesCount; i++) {
+        electionInstance.candidates(i).then(function(candidate) {
+          let id = candidate[0];
+          let name = candidate[1];
+          let voteCount = candidate[2];
+
+          //render candidate results
+          let candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
+          candidatesResults.append(candidateTemplate);
+        });
+      }
+
+      loader.hide();
+      content.show();
+    }).catch(function(error) {
+        console.warn(error);
+    });
   }
-
 };
 
 $(function() {
