7a0dcc621b7338bf1acf1eef17898f4f4c6cd601
VladisLove1 <vladislove0212@gmail.com>
2018-04-25 19:58:55 +0300

    voting func inited

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -22,4 +22,8 @@ contract Election {
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
+    function vote(uint _candidateId) public {
+        candidates[_candidateId].voteCount++;
+    }
+
 }
